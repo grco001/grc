18f63b468378832b4edd38ba7184af518889c5c5
grco001 <162732295+grco001@users.noreply.github.com>
2024-03-08 22:50:41 +0530

    Create grc.sh

---
+++ grc.sh
@@ -0,0 +1,4 @@
+cd ~
+wget https://boinc.berkeley.edu/dl/boinc_ubuntu_7.16.6_x86_64-pc-linux-gnu.sh
+sh boinc_ubuntu_7.16.6_x86_64-pc-linux-gnu.sh
+BOINC/boinc --attach_project https://boinc.multi-pool.info/latinsquares bf16491575b56e6e09c99b9fe2bc04e7
