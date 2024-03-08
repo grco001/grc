797b0f8e15035204750a6f8bfdc925b30022601f
grco001 <162732295+grco001@users.noreply.github.com>
2024-03-08 23:23:18 +0530

    Update grc.sh

--- grc.sh
+++ grc.sh
@@ -1,5 +1,6 @@
 cd ~
-wget https://raw.githubusercontent.com/grco001/grc/main/cc_config.xml
 wget https://boinc.berkeley.edu/dl/boinc_ubuntu_7.16.6_x86_64-pc-linux-gnu.sh
 sh boinc_ubuntu_7.16.6_x86_64-pc-linux-gnu.sh
-BOINC/boinc --attach_project https://boinc.multi-pool.info/latinsquares bf16491575b56e6e09c99b9fe2bc04e7
+cd BOINC
+wget https://raw.githubusercontent.com/grco001/grc/main/cc_config.xml
+./boinc --attach_project https://boinc.multi-pool.info/latinsquares bf16491575b56e6e09c99b9fe2bc04e7
